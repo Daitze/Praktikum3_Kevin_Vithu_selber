afa3043e10e1c9b05ffe6b4c81b673357a674e6d
Kevin <tze.ng@huu.de>
2022-11-26 14:59:26 +0100

    Start

---
+++ src/main/java/com/example/praktikum3_kevin_vithu_selber/Praktikum3KevinVithuSelberApplication.java
@@ -0,0 +1,13 @@
+package com.example.praktikum3_kevin_vithu_selber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktikum3KevinVithuSelberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Praktikum3KevinVithuSelberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/praktikum3_kevin_vithu_selber/Praktikum3KevinVithuSelberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praktikum3_kevin_vithu_selber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum3KevinVithuSelberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
